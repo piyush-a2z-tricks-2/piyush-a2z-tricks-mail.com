bc883f9574ac47c044ef21e4e7f1ae0d09d2bf8c
piyush-a2z-tricks-2 <chauhanji07043@gmail.com>
2025-06-17 12:38:36 +0530

    Delete models/Mail.js

--- models/Mail.js
+++
@@ -1,9 +0,0 @@
-
-const mongoose = require('mongoose');
-const MailSchema = new mongoose.Schema({
-    to: String,
-    subject: String,
-    content: String,
-    time: Date
-});
-module.exports = mongoose.model('Mail', MailSchema);
